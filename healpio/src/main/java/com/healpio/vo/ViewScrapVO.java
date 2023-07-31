3e843d3ca964770bfc6acfe047ab4f7288b7a510
kgw531 <rlarjsdn531@naver.com>
2023-07-31 09:53:12 +0900

    ==feat <package> com.healpio.vo <class> ViewScrapVO.java <content> ViewScrapVO를 추가했습니다.

---
+++ healpio/src/main/java/com/healpio/vo/ViewScrapVO.java
@@ -0,0 +1,12 @@
+package com.healpio.vo;
+
+import lombok.Data;
+
+@Data
+public class ViewScrapVO {
+	private String exercise_name;
+	private String class_title;
+	private String nickname; //강사 닉네임
+	private String address;
+	private String class_attach; //이미지
+}
