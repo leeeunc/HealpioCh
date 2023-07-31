117b311c3e677c45f414cf7a1b12a296da4c03ae
yoojin <yjhan0219@gmail.com>
2023-07-31 10:52:38 +0900

    수정 테스트

--- healpio/src/main/java/com/healpio/controller/testController.java
+++ healpio/src/main/java/com/healpio/controller/testController.java
@@ -2,15 +2,21 @@ package com.healpio.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/test/*")
 public class testController {
 	
-	// 페이지 테스트
 	@GetMapping("page")
 	public String test() {
+		return "/test/test";
+	}
+	
+	// 수정 테스트
+	@PostMapping("page")
+	public String test1() {
 		return "/test/test";
 	}
 }	
